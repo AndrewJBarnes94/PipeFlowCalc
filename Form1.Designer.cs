612c6a05c2cddbcb7ec35975fb0b5da68f85fb4b
Andrew Barnes <andrewjbarnes94@gmail.com>
2024-04-21 21:54:48 -0400

    implemented flow rate math function and basic gui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,27 @@ namespace PipeFlowCalc
         /// </summary>
         private void InitializeComponent()
         {
+            velocityTextBox = new TextBox();
             scheduleLabel = new Label();
             nominalSizeLabel = new Label();
             velocityLabel = new Label();
             scheduleComboBox = new ComboBox();
-            velocityTextBox = new TextBox();
             nominalSizeComboBox = new ComboBox();
+            calculateMaxFlowRateButton = new Button();
+            outerDiameterLabel = new Label();
+            outerDiameterTextBox = new TextBox();
+            innerDiameterLabel = new Label();
+            innerDiameterTextBox = new TextBox();
+            gpmFlowRateLabel = new Label();
+            gpmFlowRateTextBox = new TextBox();
             SuspendLayout();
+            // 
+            // velocityTextBox
+            // 
+            velocityTextBox.Location = new Point(917, 204);
+            velocityTextBox.Name = "velocityTextBox";
+            velocityTextBox.Size = new Size(250, 47);
+            velocityTextBox.TabIndex = 3;
             // 
             // scheduleLabel
             // 
@@ -66,6 +80,7 @@ namespace PipeFlowCalc
             // 
             // scheduleComboBox
             // 
+            scheduleComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
             scheduleComboBox.FormattingEnabled = true;
             scheduleComboBox.Items.AddRange(new object[] { "40", "80", "120" });
             scheduleComboBox.Location = new Point(80, 204);
@@ -74,26 +89,88 @@ namespace PipeFlowCalc
             scheduleComboBox.TabIndex = 2;
             scheduleComboBox.SelectedIndexChanged += scheduleComboBox_SelectedIndexChanged;
             // 
-            // velocityTextBox
-            // 
-            velocityTextBox.Location = new Point(917, 204);
-            velocityTextBox.Name = "velocityTextBox";
-            velocityTextBox.Size = new Size(250, 47);
-            velocityTextBox.TabIndex = 3;
-            // 
             // nominalSizeComboBox
             // 
+            nominalSizeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
             nominalSizeComboBox.FormattingEnabled = true;
             nominalSizeComboBox.Location = new Point(496, 202);
             nominalSizeComboBox.Name = "nominalSizeComboBox";
             nominalSizeComboBox.Size = new Size(302, 49);
             nominalSizeComboBox.TabIndex = 4;
             // 
+            // calculateMaxFlowRateButton
+            // 
+            calculateMaxFlowRateButton.Location = new Point(80, 302);
+            calculateMaxFlowRateButton.Name = "calculateMaxFlowRateButton";
+            calculateMaxFlowRateButton.Size = new Size(1087, 58);
+            calculateMaxFlowRateButton.TabIndex = 5;
+            calculateMaxFlowRateButton.Text = "Calculate Max Flow Rate";
+            calculateMaxFlowRateButton.UseVisualStyleBackColor = true;
+            calculateMaxFlowRateButton.Click += calculateMaxFlowRateButton_Click;
+            // 
+            // outerDiameterLabel
+            // 
+            outerDiameterLabel.AutoSize = true;
+            outerDiameterLabel.Location = new Point(77, 496);
+            outerDiameterLabel.Name = "outerDiameterLabel";
+            outerDiameterLabel.Size = new Size(223, 41);
+            outerDiameterLabel.TabIndex = 6;
+            outerDiameterLabel.Text = "Outer Diameter";
+            // 
+            // outerDiameterTextBox
+            // 
+            outerDiameterTextBox.Location = new Point(321, 496);
+            outerDiameterTextBox.Name = "outerDiameterTextBox";
+            outerDiameterTextBox.ReadOnly = true;
+            outerDiameterTextBox.Size = new Size(308, 47);
+            outerDiameterTextBox.TabIndex = 7;
+            // 
+            // innerDiameterLabel
+            // 
+            innerDiameterLabel.AutoSize = true;
+            innerDiameterLabel.Location = new Point(77, 549);
+            innerDiameterLabel.Name = "innerDiameterLabel";
+            innerDiameterLabel.Size = new Size(215, 41);
+            innerDiameterLabel.TabIndex = 8;
+            innerDiameterLabel.Text = "Inner Diameter";
+            // 
+            // innerDiameterTextBox
+            // 
+            innerDiameterTextBox.Location = new Point(321, 549);
+            innerDiameterTextBox.Name = "innerDiameterTextBox";
+            innerDiameterTextBox.ReadOnly = true;
+            innerDiameterTextBox.Size = new Size(308, 47);
+            innerDiameterTextBox.TabIndex = 9;
+            // 
+            // gpmFlowRateLabel
+            // 
+            gpmFlowRateLabel.AutoSize = true;
+            gpmFlowRateLabel.Location = new Point(77, 602);
+            gpmFlowRateLabel.Name = "gpmFlowRateLabel";
+            gpmFlowRateLabel.Size = new Size(238, 41);
+            gpmFlowRateLabel.TabIndex = 10;
+            gpmFlowRateLabel.Text = "Flow Rate (GPM)";
+            // 
+            // gpmFlowRateTextBox
+            // 
+            gpmFlowRateTextBox.Location = new Point(321, 602);
+            gpmFlowRateTextBox.Name = "gpmFlowRateTextBox";
+            gpmFlowRateTextBox.ReadOnly = true;
+            gpmFlowRateTextBox.Size = new Size(308, 47);
+            gpmFlowRateTextBox.TabIndex = 11;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1258, 723);
+            ClientSize = new Size(1258, 739);
+            Controls.Add(gpmFlowRateTextBox);
+            Controls.Add(gpmFlowRateLabel);
+            Controls.Add(innerDiameterTextBox);
+            Controls.Add(innerDiameterLabel);
+            Controls.Add(outerDiameterTextBox);
+            Controls.Add(outerDiameterLabel);
+            Controls.Add(calculateMaxFlowRateButton);
             Controls.Add(nominalSizeComboBox);
             Controls.Add(velocityTextBox);
             Controls.Add(scheduleComboBox);
@@ -119,5 +196,12 @@ namespace PipeFlowCalc
         private ComboBox scheduleComboBox;
         private TextBox velocityTextBox;
         private ComboBox nominalSizeComboBox;
+        private Button calculateMaxFlowRateButton;
+        private Label outerDiameterLabel;
+        private TextBox outerDiameterTextBox;
+        private Label innerDiameterLabel;
+        private TextBox innerDiameterTextBox;
+        private Label gpmFlowRateLabel;
+        private TextBox gpmFlowRateTextBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -41,7 +41,8 @@ namespace PipeFlowCalc
                     "20\" DN500",
                     "24\" DN600",
                 });
-            } else if (selection == "120")
+            }
+            else if (selection == "120")
             {
                 nominalSizeComboBox.Items.Clear();
                 nominalSizeComboBox.Text = "";
@@ -65,6 +66,75 @@ namespace PipeFlowCalc
                 });
             }
 
+        }
+
+        private double calculateInnerDiameterInches(double outerDiameter, double wallThickness)
+        {
+            return outerDiameter - (2 * wallThickness);
+        }
+
+        private double calculateFlowRateGPM(double velocity, double innerDiameter)
+        {
+            // Convert innerDiameter from inches to feet
+            innerDiameter = innerDiameter / 12;
+
+            // Calculate the cross sectional area
+            double crossSectionalArea = Math.PI * innerDiameter * innerDiameter / 4;
+
+            // Flow rate in ft^3/s
+            double flowRate = velocity * crossSectionalArea;
+
+            // Convert the flow rate from f^3/s to GPM and return
+            return flowRate * 7.48052 * 60;
+        }
+
+        private void calculateMaxFlowRateButton_Click(object sender, EventArgs e)
+        {
+            string scheduleComboBoxText = scheduleComboBox.Text;
+            string nominalSizeComboBoxText = nominalSizeComboBox.Text;
+            double innerDiameterInches;
+            double velocity;
+            double flowRate;
+
+            if (scheduleComboBoxText == "40") {
+                switch (nominalSizeComboBoxText)
+                {
+                    case "1/8\" DN8":
+                        innerDiameterInches = calculateInnerDiameterInches(0.41, 0.095);
+                        velocity = double.Parse(velocityTextBox.Text);
+                        flowRate = calculateFlowRateGPM(velocity, innerDiameterInches);
+                        gpmFlowRateTextBox.Text = flowRate.ToString();
+                        break;
+
+                    case "1/4\" DN12":
+                        innerDiameterInches = calculateInnerDiameterInches(0.54, 0.088);
+                        velocity = double.Parse(velocityTextBox.Text);
+                        flowRate = calculateFlowRateGPM(velocity, innerDiameterInches);
+                        gpmFlowRateTextBox.Text = flowRate.ToString();
+                        break;
+
+// Need to fill in from here
+                    case "":
+                        break;
+// To here. Then finish after 1/2 til the end.
+
+                    case "1/2\" DN15":
+                        innerDiameterInches = calculateInnerDiameterInches(0.84, 0.109);
+                        velocity = double.Parse(velocityTextBox.Text);
+                        flowRate = calculateFlowRateGPM(velocity, innerDiameterInches);
+                        gpmFlowRateTextBox.Text = flowRate.ToString();
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } else if (scheduleComboBoxText == "80") {
+
+            } else if (scheduleComboBoxText == "120") {
+
+            } else {
+            }
         }
     }
 }

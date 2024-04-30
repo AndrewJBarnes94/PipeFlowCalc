4dd135cdf6ee55df93a7ffcbdb630203b2419991
Andrew Barnes <andrewjbarnes94@gmail.com>
2024-04-29 22:54:26 -0400

    PVC and Stainless Steel Selectability with basic error handeling

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace PipeFlowCalc
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pipeFlowCalcForm));
             velocityTextBox = new TextBox();
             scheduleLabel = new Label();
             nominalSizeLabel = new Label();
@@ -38,11 +39,17 @@ namespace PipeFlowCalc
             calculateMaxFlowRateButton = new Button();
             gpmFlowRateLabel = new Label();
             gpmFlowRateTextBox = new TextBox();
+            pvcCheckBox = new CheckBox();
+            stainlessSteelCheckBox = new CheckBox();
+            menuStrip1 = new MenuStrip();
+            splitter1 = new Splitter();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // velocityTextBox
             // 
-            velocityTextBox.Location = new Point(917, 312);
+            velocityTextBox.Location = new Point(924, 450);
             velocityTextBox.Name = "velocityTextBox";
             velocityTextBox.Size = new Size(250, 47);
             velocityTextBox.TabIndex = 3;
@@ -50,7 +57,7 @@ namespace PipeFlowCalc
             // scheduleLabel
             // 
             scheduleLabel.AutoSize = true;
-            scheduleLabel.Location = new Point(80, 268);
+            scheduleLabel.Location = new Point(87, 406);
             scheduleLabel.Name = "scheduleLabel";
             scheduleLabel.Size = new Size(139, 41);
             scheduleLabel.TabIndex = 0;
@@ -59,7 +66,7 @@ namespace PipeFlowCalc
             // nominalSizeLabel
             // 
             nominalSizeLabel.AutoSize = true;
-            nominalSizeLabel.Location = new Point(496, 268);
+            nominalSizeLabel.Location = new Point(503, 406);
             nominalSizeLabel.Name = "nominalSizeLabel";
             nominalSizeLabel.Size = new Size(191, 41);
             nominalSizeLabel.TabIndex = 0;
@@ -68,7 +75,7 @@ namespace PipeFlowCalc
             // velocityLabel
             // 
             velocityLabel.AutoSize = true;
-            velocityLabel.Location = new Point(917, 268);
+            velocityLabel.Location = new Point(924, 406);
             velocityLabel.Name = "velocityLabel";
             velocityLabel.Size = new Size(192, 41);
             velocityLabel.TabIndex = 1;
@@ -78,8 +85,7 @@ namespace PipeFlowCalc
             // 
             scheduleComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
             scheduleComboBox.FormattingEnabled = true;
-            scheduleComboBox.Items.AddRange(new object[] { "40", "80", "120" });
-            scheduleComboBox.Location = new Point(80, 312);
+            scheduleComboBox.Location = new Point(87, 450);
             scheduleComboBox.Name = "scheduleComboBox";
             scheduleComboBox.Size = new Size(302, 49);
             scheduleComboBox.TabIndex = 2;
@@ -89,14 +95,14 @@ namespace PipeFlowCalc
             // 
             nominalSizeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
             nominalSizeComboBox.FormattingEnabled = true;
-            nominalSizeComboBox.Location = new Point(496, 310);
+            nominalSizeComboBox.Location = new Point(503, 448);
             nominalSizeComboBox.Name = "nominalSizeComboBox";
             nominalSizeComboBox.Size = new Size(302, 49);
             nominalSizeComboBox.TabIndex = 4;
             // 
             // calculateMaxFlowRateButton
             // 
-            calculateMaxFlowRateButton.Location = new Point(82, 452);
+            calculateMaxFlowRateButton.Location = new Point(89, 590);
             calculateMaxFlowRateButton.Name = "calculateMaxFlowRateButton";
             calculateMaxFlowRateButton.Size = new Size(1087, 58);
             calculateMaxFlowRateButton.TabIndex = 5;
@@ -107,7 +113,7 @@ namespace PipeFlowCalc
             // gpmFlowRateLabel
             // 
             gpmFlowRateLabel.AutoSize = true;
-            gpmFlowRateLabel.Location = new Point(310, 161);
+            gpmFlowRateLabel.Location = new Point(314, 86);
             gpmFlowRateLabel.Name = "gpmFlowRateLabel";
             gpmFlowRateLabel.Size = new Size(238, 41);
             gpmFlowRateLabel.TabIndex = 10;
@@ -115,17 +121,69 @@ namespace PipeFlowCalc
             // 
             // gpmFlowRateTextBox
             // 
-            gpmFlowRateTextBox.Location = new Point(554, 161);
+            gpmFlowRateTextBox.Location = new Point(558, 86);
             gpmFlowRateTextBox.Name = "gpmFlowRateTextBox";
             gpmFlowRateTextBox.ReadOnly = true;
             gpmFlowRateTextBox.Size = new Size(308, 47);
             gpmFlowRateTextBox.TabIndex = 11;
             // 
+            // pvcCheckBox
+            // 
+            pvcCheckBox.AutoSize = true;
+            pvcCheckBox.Location = new Point(10, 56);
+            pvcCheckBox.Name = "pvcCheckBox";
+            pvcCheckBox.Size = new Size(110, 45);
+            pvcCheckBox.TabIndex = 12;
+            pvcCheckBox.Text = "PVC";
+            pvcCheckBox.UseVisualStyleBackColor = true;
+            pvcCheckBox.CheckedChanged += pvcCheckBox_CheckedChanged;
+            // 
+            // stainlessSteelCheckBox
+            // 
+            stainlessSteelCheckBox.AutoSize = true;
+            stainlessSteelCheckBox.Location = new Point(424, 56);
+            stainlessSteelCheckBox.Name = "stainlessSteelCheckBox";
+            stainlessSteelCheckBox.Size = new Size(241, 45);
+            stainlessSteelCheckBox.TabIndex = 13;
+            stainlessSteelCheckBox.Text = "Stainless Steel";
+            stainlessSteelCheckBox.UseVisualStyleBackColor = true;
+            stainlessSteelCheckBox.CheckedChanged += stainlessSteelCheckBox_CheckedChanged;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(40, 40);
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1264, 24);
+            menuStrip1.TabIndex = 14;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // splitter1
+            // 
+            splitter1.Location = new Point(0, 24);
+            splitter1.Name = "splitter1";
+            splitter1.Size = new Size(8, 742);
+            splitter1.TabIndex = 15;
+            splitter1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(stainlessSteelCheckBox);
+            groupBox1.Controls.Add(pvcCheckBox);
+            groupBox1.Location = new Point(79, 214);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1097, 124);
+            groupBox1.TabIndex = 16;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Pipe Material";
+            // 
             // pipeFlowCalcForm
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1264, 685);
+            ClientSize = new Size(1264, 766);
+            Controls.Add(groupBox1);
+            Controls.Add(splitter1);
             Controls.Add(gpmFlowRateTextBox);
             Controls.Add(gpmFlowRateLabel);
             Controls.Add(calculateMaxFlowRateButton);
@@ -135,8 +193,13 @@ namespace PipeFlowCalc
             Controls.Add(velocityLabel);
             Controls.Add(nominalSizeLabel);
             Controls.Add(scheduleLabel);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
             Name = "pipeFlowCalcForm";
             Text = "Pipe Flow Calculator";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -157,5 +220,10 @@ namespace PipeFlowCalc
         private Button calculateMaxFlowRateButton;
         private Label gpmFlowRateLabel;
         private TextBox gpmFlowRateTextBox;
+        private CheckBox pvcCheckBox;
+        private CheckBox stainlessSteelCheckBox;
+        private MenuStrip menuStrip1;
+        private Splitter splitter1;
+        private GroupBox groupBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,16 @@ namespace PipeFlowCalc
         public pipeFlowCalcForm()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(formKeyDown);
+        }
+
+        public void formKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                calculateMaxFlowRateButton_Click(this, new EventArgs());
+            }
         }
 
         private void scheduleComboBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -65,6 +75,37 @@ namespace PipeFlowCalc
                     "12\" DN300",
                 });
             }
+            else if (selection == "10")
+            {
+                nominalSizeComboBox.Items.Clear();
+                nominalSizeComboBox.Text = "";
+                nominalSizeComboBox.Items.AddRange(new object[]
+                {
+                    "1/8\"",
+                    "1/4\"",
+                    "3/8\"",
+                    "1/2\"",
+                    "3/4\"",
+                    "1\"",
+                    "1 1/4\"",
+                    "1 1/2\"",
+                    "2\"",
+                    "2 1/2\"",
+                    "3\"",
+                    "3 1/2\"",
+                    "4\"",
+                    "5\"",
+                    "6\"",
+                    "8\"",
+                    "10\"",
+                    "12\"",
+                    "14\"",
+                    "16\"",
+                    "18\"",
+                    "20\"",
+                    "24\"",
+                });
+            }
 
         }
 
@@ -72,7 +113,7 @@ namespace PipeFlowCalc
         {
             // Calculate innder diameter in inches
             double innerDiameter = outerDiameter - (2 * wallThickness);
-            
+
             // Convert innerDiameter from inches to feet
             innerDiameter = innerDiameter / 12;
 
@@ -84,16 +125,29 @@ namespace PipeFlowCalc
 
             // Convert the flow rate from f^3/s to GPM 
             flowRate = flowRate * 7.48052 * 60;
-            
+
             // Update gpmFlowRateTextBox
             gpmFlowRateTextBox.Text = flowRate.ToString();
         }
 
         private void calculateMaxFlowRateButton_Click(object sender, EventArgs e)
         {
+            // Check if any of the required fields are empty
+            if (string.IsNullOrEmpty(scheduleComboBox.Text) || string.IsNullOrEmpty(nominalSizeComboBox.Text) || string.IsNullOrEmpty(velocityTextBox.Text))
+            {
+                MessageBox.Show("Please ensure all fields are filled out.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Safe parsing of velocity
+            if (!double.TryParse(velocityTextBox.Text, out double velocity))
+            {
+                MessageBox.Show("Please enter a valid number for velocity.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             string scheduleComboBoxText = scheduleComboBox.Text;
             string nominalSizeComboBoxText = nominalSizeComboBox.Text;
-            double velocity = double.Parse(velocityTextBox.Text);
 
             if (scheduleComboBoxText == "40")
             {
@@ -362,8 +416,148 @@ namespace PipeFlowCalc
 
                 }
             }
+            else if (scheduleComboBoxText == "10")
+            {
+                switch (nominalSizeComboBoxText)
+                {
+                    case "1/8\"":
+                        gpmFlowRate(0.405, 0.049, velocity);
+                        break;
+
+                    case "1/4\"":
+                        gpmFlowRate(0.540, 0.065, velocity);
+                        break;
+
+                    case "3/8\"":
+                        gpmFlowRate(0.675, 0.065, velocity);
+                        break;
+
+                    case "1/2\"":
+                        gpmFlowRate(0.840, 0.083, velocity);
+                        break;
+
+                    case "3/4\"":
+                        gpmFlowRate(1.050, 0.083, velocity);
+                        break;
+
+                    case "1\"":
+                        gpmFlowRate(1.315, 0.109, velocity);
+                        break;
+
+                    case "1 1/4\"":
+                        gpmFlowRate(1.660, 0.109, velocity);
+                        break;
+
+                    case "1 1/2\"":
+                        gpmFlowRate(1.900, 0.109, velocity);
+                        break;
+
+                    case "2\"":
+                        gpmFlowRate(2.375, 0.109, velocity);
+                        break;
+
+                    case "2 1/2\"":
+                        gpmFlowRate(2.875, 0.120, velocity);
+                        break;
+
+                    case "3\"":
+                        gpmFlowRate(3.500, 0.120, velocity);
+                        break;
+
+                    case "3 1/2\"":
+                        gpmFlowRate(4.000, 0.120, velocity);
+                        break;
+
+                    case "4\"":
+                        gpmFlowRate(4.500, 0.120, velocity);
+                        break;
+
+                    case "5\"":
+                        gpmFlowRate(5.563, 0.134, velocity);
+                        break;
+
+                    case "6\"":
+                        gpmFlowRate(6.625, 0.134, velocity);
+                        break;
+
+                    case "8\"":
+                        gpmFlowRate(8.625, 0.148, velocity);
+                        break;
+
+                    case "10\"":
+                        gpmFlowRate(10.750, 0.165, velocity);
+                        break;
+
+                    case "12\"":
+                        gpmFlowRate(12.750, 0.180, velocity);
+                        break;
+
+                    case "14\"":
+                        gpmFlowRate(14.000, 0.188, velocity);
+                        break;
+
+                    case "16\"":
+                        gpmFlowRate(16.000, 0.188, velocity);
+                        break;
+
+                    case "18\"":
+                        gpmFlowRate(18.000, 0.188, velocity);
+                        break;
+
+                    case "20\"":
+                        gpmFlowRate(20.000, 0.218, velocity);
+                        break;
+
+                    case "22\"":
+                        gpmFlowRate(22.000, 0.218, velocity);
+                        break;
+
+                    case "24\"":
+                        gpmFlowRate(24.000, 0.250, velocity);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void pvcCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (pvcCheckBox.Checked)
+            {
+                gpmFlowRateTextBox.Text = "";
+                stainlessSteelCheckBox.Checked = false;
+                nominalSizeComboBox.Items.Clear();
+                scheduleComboBox.Items.Clear();
+                scheduleComboBox.Text = "";
+                scheduleComboBox.Items.AddRange(new object[]
+                {
+                   40,
+                   80,
+                   120,
+                });
+            }
             else
             {
+                scheduleComboBox.Items.Clear();
+            }
+            
+        }
+
+        private void stainlessSteelCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (stainlessSteelCheckBox.Checked)
+            {
+                gpmFlowRateTextBox.Text = "";
+                pvcCheckBox.Checked = false;
+                nominalSizeComboBox.Items.Clear();
+                scheduleComboBox.Items.Clear();
+                scheduleComboBox.Text = "";
+                scheduleComboBox.Items.AddRange(new object[]
+                {
+                    10,
+                });
             }
         }
     }
